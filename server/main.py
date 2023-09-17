8dc3e53f04a6f300febeacbaab4d55880cb936f3
prajwalshah19 <pjpackers99@gmail.com>
2023-09-16 21:51:07 -0400

    updated main.py

--- server/main.py
+++ server/main.py
@@ -1,7 +1,8 @@
 import openai
-from flask import Flask
-from Keys import API_KEY
-app = Flask(__name__)
+import os
+from flask import Flask, request, send_from_directory
+#from Keys import API_KEY
+app = Flask(__name__, static_folder="../client/pvet/build")
 
 '''
 openai.api_key = API_KEY
@@ -22,11 +23,30 @@ print(output)
 # print(type(output))
 '''
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
 
 @app.route("/generation")
 def generation():
-    return {"generation": ["generation1", "generation2", "generation3"]}
+    inputs = get_inputs()
+    question = f"Where are some places I can go {inputs[0]} in {inputs[1]}?"
+    print(question)
 
+    return {"generation": [question, "generation2", "generation3"]}
+
+@app.route("/get_inputs", methods = ['POST'])
+def get_inputs():
+    activity = request.form.get('activity')
+    print("looking for activity", activity)
+    location = request.form.get('location')
+    print("looking for location", location)
+    return [activity, location]
 
 if __name__ == "__main__":
     app.run(debug=True)
