e721014488698d0b625edba233c29ec0ca86af6e
prajwalshah19 <pjpackers99@gmail.com>
2023-09-16 21:30:12 -0400

    i have no clue wtf happend but it works

--- client/pvet/src/Pages/Landing.js
+++ client/pvet/src/Pages/Landing.js
@@ -6,6 +6,7 @@ function Landing() {
 
   const [activity, setActivity] = useState("")
   const [loc, setLoc] = useState("")
+  
 
   const handleActivityChange = (event) => {
     setActivity(event.target.value);
@@ -22,9 +23,9 @@ function Landing() {
 
   let navigate = useNavigate()
     
-  function onButtonClick() {
-      navigate('/content')
-  }
+  // function onButtonClick() {
+  //     navigate('/content')
+  // }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -32,26 +33,31 @@ function Landing() {
       
       <h1 class="text-7xl font-bold text-orange-950 text-center mb-3">Hello, </h1>
       <div className = "flex flex-col w-2/3 items-center justify-center">
-      <input
-        type="text"
-        class="w-full border-2 border-orange-950 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none mb-2"
-        placeholder="What would you like to do this weekend?" 
-        onChange={handleActivityChange}
-      />
-      {activity != "" ? 
-      <input
-        type="text"
-        class="w-full border-2 border-orange-950 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none mb-2"
-        placeholder= {"Where would you like to " + activity + "?"}
-        onChange={handleLocationChange}
-      />
+        <form className = "flex flex-col w-2/3 items-center justify-center" method='POST' action='/get_inputs'>
+          <input
+            type="text"
+            class="w-full border-2 border-orange-950 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none mb-2"
+            placeholder="What would you like to do this weekend?" 
+            id = "activity"
+            name="activity"
+            onChange={handleActivityChange}
+          />
+          {activity != "" ? 
+          <input
+            type="text"
+            class="w-full border-2 border-orange-950 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none mb-2"
+            placeholder= {"Where would you like to " + activity + "?"}
+            id = "location"
+            name="location"
+            onChange={handleLocationChange}
+          />
 
-      : <></>}
-      {loc != "" && activity != "" ? 
-      <button type="submit" className="bg-orange-950 text-white h-10 px-4 rounded-lg" onClick = {onButtonClick}>Find Places</button>
+          : <></>}
+          {loc != "" && activity != "" ? 
+          <button type="submit" className="bg-orange-950 text-white h-10 px-4 rounded-lg">Find Places</button>
 
-      : <></>}
-
+          : <></>}
+      </form>
 
 
       </div>
