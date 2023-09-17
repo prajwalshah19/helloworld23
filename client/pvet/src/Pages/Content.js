0178e9e01c571ba8ddcdee15442b8c74414f2783
Tristan Sze <tristan@pal-nat186-22-222.itap.purdue.edu>
2023-09-17 00:21:10 -0400

    new branch creation

--- client/pvet/src/Pages/Content.js
+++ client/pvet/src/Pages/Content.js
@@ -3,9 +3,28 @@ import { useState } from 'react';
 import LocCard from '../Components/LocCard';
 function Content() {
 
+    const sampleData = [
+        {
+            name: "Grant Park",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+            name: "Grant Park",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+            name: "Grant Park",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+    ]
     return (
-       <div className = "flex">
-           <LocCard />
+       <div className = "flex xs:flex-col m-5">
+           {sampleData.map( data => { return (
+            <LocCard
+                name = {data.name}
+                description = {data.description}
+            />
+           )})}
        </div>
          
 
